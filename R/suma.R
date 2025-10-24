5fc58b4d67a59119048560f09e0b658701e8e7fc
Gaspi Alfonso <galfonso1@mail.austral.edu.ar>
2025-10-24 10:20:13 -0300

    Agrega documentación de Suma.

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,16 @@
+#' Suma dos números
+#'
+#' Suma dos números numéricos y positivos.
+#'
+#' @param x Un número positivo (no negativo).
+#' @param y Un número positivo (no negativo).
+#'
+#' @returns Un vector numérico.
+#'
+#' @examples
+#' suma(1,1)
+#' suma(3,8)
+#' @export
 suma <- function(x = 0, y = 0) {
   # Verificar que sean numéricos
   if (!is.numeric(x) || !is.numeric(y)) {
